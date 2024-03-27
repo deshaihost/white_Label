9f971aeef8ca85a10ee3bb3de0f645d63aeee626
testexpinator <testexpinator@gmail.com>
2024-03-27 18:25:24 +0530

    completed home page design

--- src/pages/home/works/Works.jsx
+++ src/pages/home/works/Works.jsx
@@ -10,23 +10,25 @@ const Works = () => {
     return(
         <section className='works'>
             <div className="works-heading">
-                <div className="speed-circle"></div>
-                <h2>How it Works 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="18" viewBox="0 0 19 18" fill="none">
-                        <path d="M9.5 0L10.7092 7.79085L18.5 9L10.7092 10.2092L9.5 18L8.29085 10.2092L0.5 9L8.29085 7.79085L9.5 0Z" fill="url(#paint0_linear_27_486)"></path>
-                        <path d="M15.5 12L16.094 14.406L18.5 15L16.094 15.594L15.5 18L14.906 15.594L12.5 15L14.906 14.406L15.5 12Z" fill="url(#paint1_linear_27_486)"></path>
-                        <defs>
-                            <linearGradient id="paint0_linear_27_486" x1="9.5" y1="0" x2="9.5" y2="18" gradientUnits="userSpaceOnUse">
-                                <stop stop-color="#F9FBFF"></stop>
-                                <stop offset="1" stop-color="#6CA7FF"></stop>
-                            </linearGradient>
-                            <linearGradient id="paint1_linear_27_486" x1="15.5" y1="12" x2="15.5" y2="18" gradientUnits="userSpaceOnUse">
-                                <stop stop-color="#F9FBFF"></stop>
-                                <stop offset="1" stop-color="#6CA7FF"></stop>
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                </h2>
+                <Container>
+                    <div className="speed-circle"></div>
+                    <h2>How it Works 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="18" viewBox="0 0 19 18" fill="none">
+                            <path d="M9.5 0L10.7092 7.79085L18.5 9L10.7092 10.2092L9.5 18L8.29085 10.2092L0.5 9L8.29085 7.79085L9.5 0Z" fill="url(#paint0_linear_27_486)"></path>
+                            <path d="M15.5 12L16.094 14.406L18.5 15L16.094 15.594L15.5 18L14.906 15.594L12.5 15L14.906 14.406L15.5 12Z" fill="url(#paint1_linear_27_486)"></path>
+                            <defs>
+                                <linearGradient id="paint0_linear_27_486" x1="9.5" y1="0" x2="9.5" y2="18" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#F9FBFF"></stop>
+                                    <stop offset="1" stop-color="#6CA7FF"></stop>
+                                </linearGradient>
+                                <linearGradient id="paint1_linear_27_486" x1="15.5" y1="12" x2="15.5" y2="18" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#F9FBFF"></stop>
+                                    <stop offset="1" stop-color="#6CA7FF"></stop>
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                    </h2>
+                </Container>
             </div>
             <Container>
                 <div className="works-container">
@@ -57,7 +59,7 @@ const Works = () => {
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="works-content">
-                                <h4><strong>Full </strong> Host Control</h4>
+                                <h4>Full <strong> Host Control</strong> </h4>
                                 <p>Experience full host control with HostBuddy’s 24/7 Support. Here’s what it means for you</p>
                             </div>
                         </div>
