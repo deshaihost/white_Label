11cb7694cf1b5c645949d49a6151235a268a2151
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-05-16 16:44:43 -0700

    minor styling change

--- src/pages/properties/listIntegrationProperties/ListIntegrationProperties.jsx
+++ src/pages/properties/listIntegrationProperties/ListIntegrationProperties.jsx
@@ -322,7 +322,7 @@ const ListIntegrationProperties = () => {
                                 <CiCalendar className="text-primary" />
                               </Button>
                             </div>
-                            <h6 style={{ marginTop:'13px', color: 'rgb(195,195,195)', fontSize:'0.84em', fontWeight:'normal' }}>
+                            <h6 style={{ marginTop:'13px', color: 'rgb(135,135,135)', fontSize:'0.84em', fontWeight:'normal' }}>
                               {PropertiesExtraData?.[properties]?.status_statement.split(' ').map((word, index) => 
                                 <React.Fragment key={index}>
                                   {word === 'RESPONDING' ? <span style={{ color: 'rgb(0,200,0)' }}>{word}</span> :
