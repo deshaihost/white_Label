c7ed67ed561715a9a33b937cc26611a3e7121c4c
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-06-27 13:10:21 -0700

    Update blog landing page styling for mobile

--- src/blog/blogLanding.jsx
+++ src/blog/blogLanding.jsx
@@ -8,8 +8,8 @@ const automate_str_thumbnail = "https://hostbuddylb.com/blog/automate_str_6-25/t
 const BlogLandingPage = () => {
 
     const articles = [
-      { id:"automate_str_6-25", title:"How to Automate Your Short Term Rental Business", date:"Jun 25, 2024", img:automate_str_thumbnail, description:"In this post, we'll dive into the essentials of short term rental automation, from integrating a Property Management System (PMS) to implementing artificial intelligence for guest communication." },
-      { id:"5_best_PMS_6-13", title:"5 Best Property Management Software of 2024 (Updated)", date:"Jun 13, 2024", img:five_best_thumbnail, description:"Are you ready to revolutionize your short term rental management game? Today, we're diving into the realm of Property Management Software (PMS) to unveil the best solutions of 2024." },
+      { id:"automate_str_6-25", title:"How to Automate Your Short Term Rental Business", date:"Jun 25, 2024", img:automate_str_thumbnail, description:"STR automation mastery: Uncover tech-driven solutions, AI-powered tools, and clever hacks to minimize effort and maximize returns for your short-term rentals." },
+      { id:"5_best_PMS_6-13", title:"5 Best Property Management Software of 2024 (Updated)", date:"Jun 13, 2024", img:five_best_thumbnail, description:"Elevate your STR game with 2024's best management software. Find out which tools offer the latest tech and clever automation for hosts." },
     ];
 
     return (
