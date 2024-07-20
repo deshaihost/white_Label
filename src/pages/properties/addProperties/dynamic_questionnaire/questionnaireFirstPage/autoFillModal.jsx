28431bef028d8ca260d3621044b5bb54074110b4
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-07-20 10:24:54 -0700

    Fix source selection checkbox logic in auto fill popup

--- src/pages/properties/addProperties/dynamic_questionnaire/questionnaireFirstPage/autoFillModal.jsx
+++ src/pages/properties/addProperties/dynamic_questionnaire/questionnaireFirstPage/autoFillModal.jsx
@@ -13,30 +13,33 @@ const AutoFillModal = ({ handleClose, show, apiPropertyData }) => {
   const [integrationSources, setIntegrationSources] = useState([]);
   const [fileSources, setFileSources] = useState([]);
   const [conversationDataSources, setConversationDataSources] = useState([]);
-  const [sources, setSources] = useState([ { id: 'source1', name: 'Source 1' }, { id: 'source2', name: 'Source 2' }, { id: 'source3', name: 'Source 3' } ]);
-  const [selectedSources, setSelectedSources] = useState(sources.map(source => source.id)); // all selected by default
+  const [allSources, setAllSources] = useState([]);
+  const [selectedSources, setSelectedSources] = useState(allSources.map(source => source.id)); // all selected by default
   const [propertyName, setPropertyName] = useState(null);
   const [autoFillApiLoading, setAutoFillApiLoading] = useState(false);
 
   // When apiPropertyData populates (from parent), populate the sources
   useEffect(() => {
     if (apiPropertyData) {
-      const integrationSources = Object.keys(apiPropertyData.supporting_doc_items?.integration_data || {}); // should just be one if an integration is linked, or none otherwise
-      const fileSources = Object.keys(apiPropertyData?.supporting_doc_items?.file_data || {});
-      const conversationDataSources = apiPropertyData?.supporting_doc_items?.conversation_data?.months && Object.keys(apiPropertyData.supporting_doc_items.conversation_data.months).length > 0 ? ["Conversation history"] : [];
-      const allSources = [...integrationSources, ...conversationDataSources, ...fileSources];
-      setSources(allSources.map(source => ({ id: source, name: source })));
-      setFileSources(fileSources);
-      setIntegrationSources(integrationSources);
-      setConversationDataSources(conversationDataSources);
-      setSelectedSources(allSources);
+      const allIntegrationSources = Object.keys(apiPropertyData.supporting_doc_items?.integration_data || {}); // should just be one if an integration is linked, or none otherwise
+      const allFileSources = Object.keys(apiPropertyData?.supporting_doc_items?.file_data || {});
+      const allConversationDataSources = (apiPropertyData?.supporting_doc_items?.conversation_data?.months && Object.keys(apiPropertyData.supporting_doc_items.conversation_data.months).length > 0) ? ["Conversation history"] : [];
+      const allSourcesFound = [...allIntegrationSources, ...allConversationDataSources, ...allFileSources];
+      setAllSources(allSourcesFound.map(source => ({
+        id: source,
+        name: allIntegrationSources.includes(source) ? source.charAt(0).toUpperCase() + source.slice(1).replace(/_/g, ' ') : source // for integration sources, capitalize the first letter and replace underscores with spaces (in the user-facing label only)
+      })));
+      setFileSources(allFileSources);
+      setIntegrationSources(allIntegrationSources);
+      setConversationDataSources(allConversationDataSources);
+      setSelectedSources(allSourcesFound);
       setPropertyName(apiPropertyData.property_name);
     }
   }, [apiPropertyData]);
 
-  const handleCheckboxChange = (sourceId) => {
+  const handleCheckboxChange = (sourceId, isChecked) => {
     setSelectedSources(prev => {
-      if (prev.includes(sourceId)) {
+      if (!isChecked) {
         return prev.filter(id => id !== sourceId);
       } else {
         return [...prev, sourceId];
@@ -61,7 +64,13 @@ const AutoFillModal = ({ handleClose, show, apiPropertyData }) => {
           headers: { Authorization: `Bearer ${token}`, "X-API-Key": API_KEY },
           validateStatus: function (status) { return status >= 200 && status < 500; } // don't throw an error for non-2xx responses
         };
-        const json_body = { 'docs_to_use':{'integration_data':integrationSources, 'file_data':fileSources, 'conversation_data':conversationDataSources} };
+        const json_body = {
+          'docs_to_use': {
+            'integration_data': integrationSources.filter(source => selectedSources.includes(source)),
+            'file_data': fileSources.filter(source => selectedSources.includes(source)),
+            'conversation_data': conversationDataSources.filter(source => selectedSources.includes(source))
+          }
+        };
         const response = await axios.post(`${baseUrl}/properties/${propertyName}/auto_fill_questionnaire`, json_body, config);
 
         if (response.status === 200) {
@@ -85,18 +94,18 @@ const AutoFillModal = ({ handleClose, show, apiPropertyData }) => {
             <div className="text-center">
               <p>You can auto-fill the property details form using the information in the PMS, and from any uploaded documents. This can make HostBuddy's knowledge base easier to visualize and manage.</p>
               <p>If you have entered any data into the form, auto-fill will try to preserve that information while adding any new details. It may take about a minute to complete.</p>
-              {sources.length > 0 ? (
+              {allSources.length > 0 ? (
                 <p style={{marginBottom:'20px'}}>Choose the sources you want to use for auto-fill:</p>
               ) : (
                 <p>To use auto-fill, link this property to a PMS listing or upload a document.</p>
               )}
             </div>
 
-            {sources.length > 0 && (
+            {allSources.length > 0 && (
               <div className="sources-container">
-                {sources.map(source => (
+                {allSources.map(source => (
                   <div key={source.id}>
-                    <input className="form-check-input" type="checkbox" value={source.id} id={source.id} checked={selectedSources.includes(source.id)} onChange={() => handleCheckboxChange(source.id)}/>
+                    <input className="form-check-input" type="checkbox" value={source.id} id={source.id} checked={selectedSources.includes(source.id)} onChange={(e) => handleCheckboxChange(source.id, e.target.checked)}/>
                     <label className="form-check-label" htmlFor={source.id}>
                       {source.name}
                     </label>
@@ -106,7 +115,7 @@ const AutoFillModal = ({ handleClose, show, apiPropertyData }) => {
             )}
             
             {!autoFillApiLoading ? (
-              sources.length > 0 && selectedSources.length > 0 && (
+              allSources.length > 0 && selectedSources.length > 0 && (
                 <div className="text-center">
                   <button className="mw-auto btn btn-primary text-white border border-primary rounded-pill px-5 mt-3" onClick={() => callAutoFillApi()}>
                     Start Auto-Fill
