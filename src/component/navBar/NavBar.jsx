522e7f71c25748482c10b6de3840af87502d0a72
webmanjot01 <webmanjot01@gmail.com>
2024-07-30 14:54:03 +0530

    feat:structure

--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -45,7 +45,7 @@ const NavBar = () => {
       const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); // time remaining, in full days
 
       if (expiryDate > currentDate) { // Payment standing is bad, but user still has grace period before services are paused
-        return ( <Alert variant="danger">{" "}Your last subscription payment didn't go through. Please click on "Subscription" in your <Link to="/account">Account page</Link> to update your payment info. Otherwise, your services will be paused in{" "}{diffDays} days.{" "}</Alert> );
+        return (<Alert variant="danger">{" "}Your last subscription payment didn't go through. Please click on "Subscription" in your <Link to="/account">Account page</Link> to update your payment info. Otherwise, your services will be paused in{" "}{diffDays} days.{" "}</Alert>);
       } else { // Payment standing is bad, and grace period is over. Services have been paused.
         return (<Alert variant="danger">{" "}Your last subscription payment didn't go through and your services have been paused. Please click on "Subscription" in your{" "} <Link to="/account">Account page</Link> to update your payment info.{" "}</Alert>);
       }
@@ -153,6 +153,9 @@ const NavBar = () => {
                   </NavLink>
                   <NavLink exact to="/setup-guide" className="nav-link" activeClassName="active" onClick={handleNavLinkClick}>
                     Setup Guide
+                  </NavLink>
+                  <NavLink exact to="/getstarted" className="nav-link" activeClassName="active" onClick={handleNavLinkClick}>
+                    Get Started
                   </NavLink>
                 </>
               ) : (
---
+++ src/pages/getStarted/GetStarted.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GetStarted = () => {
+    return (
+        <>
+            GetStarted
+        </>
+    )
+}
+
+export default GetStarted
