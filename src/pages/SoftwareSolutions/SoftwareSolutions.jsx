1f10924ee1420c228b81572f6c39f224d9cebd14
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-08-19 12:15:42 -0700

    Manjot changes - whitelabel page and get started scalability fix

--- src/pages/SoftwareSolutions/SoftwareSolutions.jsx
+++ src/pages/SoftwareSolutions/SoftwareSolutions.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import "../becomen Affiliate/BecomeAnAffiliate.css"
 import { Col, Container, Row } from 'react-bootstrap'
-import ContactUs from "../testProperty/discover/contactUs/ContactUs";
-import Horse from "../../images/horse.png"
-import Rating from "../../images/rating.png"
+
+
 const LOGO = 'https://hostbuddylb.com/logo/logo_footer.webp';
 const Earn = 'https://hostbuddylb.com/becomeAnAffiliate/Commision.webp';
 const BecomeAff = 'https://hostbuddylb.com/becomeAnAffiliate/earn-affilate.webp';
+const Horse = 'https://hostbuddylb.com/becomeAnAffiliate/horse.webp';
+const Rating = 'https://hostbuddylb.com/becomeAnAffiliate/rating.webp';
 
 const SoftwareSolutions = () => {
-    const [modalShow, setModalShow] = useState(false);
+    const [modalShow, setModalShow] = useState(false); // unused in this file
     return (
         <>
             <div className=''>
@@ -20,18 +21,10 @@ const SoftwareSolutions = () => {
                         <Row className="mt-3 align-items-center px-2 md-px-0 software-inner">
                             <Col md={7} className='custom-order-2 pe-0 pe-md-5 mt-3 mb-md-0'>
                                 <h5 className='text-uppercase fw-normal text-white mb-4'>Integrate powerful AI guest communication technology directly into your product offering</h5>
-                                <p className='text-white fw-normal'>
-                                    <strong className='text-white '>Cutting-Edge Technology: </strong>
-                                    Offer your users access to the most advanced AI solution for short-term rental hosts.</p>
-                                <p className='text-white fw-bold lg:my-5 my-3 '><strong>Out-of-the-Box Solution:</strong>
-                                Our technology is already developed and perfected. Save your business time and money by starting at the finish line.</p>
-                                <p className='text-white fw-normal  lg:mb-5 mb-3'>
-                                    <strong className='text-white'>Stay Ahead of the Competition:</strong>
-                                    Leverage our technology as your own solution and establish yourself as a leader in the industry.</p>
-                                <p className='text-white fw-normal '>
-                                    <strong className='text-white'>Additional Revenue Stream and Retention:</strong>
-                                    Generate a new stream of revenue by offering advanced AI features at a premium. Convert new customers and keep existing customers satisfied.</p>
-                                <button onClick={() => setModalShow(true)} className='emp-btn fw-bold text-black  w-100 md:my-5 mb-0 mt-5    display-btn'>EMPOWER HOSTS WITH AI </button>
+                                <p className='text-white fw-normal'><strong className='text-white '>Cutting-Edge Technology: </strong> Offer your users access to the most advanced AI solution for short-term rental hosts.</p>
+                                <p className='text-white fw-bold lg:my-5 my-3'><strong>Out-of-the-Box Solution:</strong> Our technology is already developed and perfected. Save your business time and money by starting at the finish line.</p>
+                                <p className='text-white fw-normal lg:mb-5 mb-3'><strong className='text-white'>Stay Ahead of the Competition:</strong> Leverage our technology as your own solution and establish yourself as a leader in the industry.</p>
+                                <p className='text-white fw-normal'><strong className='text-white'>Additional Revenue Stream and Retention:</strong> Generate a new stream of revenue by offering advanced AI features at a premium. Convert new customers and keep existing customers satisfied.</p>
                             </Col>
                             <Col md={5} className='pe-md-5 custom-order-1 text-center'>
                                 <img className='w-75' src={Horse} />
@@ -39,12 +32,12 @@ const SoftwareSolutions = () => {
                         </Row>
                     </Container >
                 </div >
-                <div className='earn-commision md:py-5 py-3 custom-sections'>
+                <div className='earn-commision custom-sections software-solution'>
                     <Container>
                         <Row className="mt-3 align-items-center px-2 md-px-0">
                             <Col md={5} className='pe-md-5  text-center'>
                                 <img className='w-75' src={Earn} />
-                                <button onClick={() => setModalShow(true)} className='emp-btn fw-bold text-black fs-3 w-100 lg:my-5 my-3 display-btn-new'>PROVIDE MORE VALUE</button></Col>
+                                <button className='emp-btn fw-bold text-black fs-3 w-100 lg:my-5 my-3 display-btn-new'>PROVIDE MORE VALUE</button></Col>
                             <Col md={7} className=' ps-md-5 pt-3 pt-md-0 '>
                                 <h1 className='text-uppercase fw-normal text-white xl:mb-5 mb-3'>HostBuddy AI Technology</h1>
                                 <p className='text-white'><strong>Comprehensive Communication Coverage: </strong>Enable hosts to leverge advanced AI technology to respond directly to guests on their behalf, or provide suggested responses to xpedite communication.</p>
@@ -60,13 +53,12 @@ const SoftwareSolutions = () => {
                                 <p className='text-white fw-bold'><strong>Early Feature Access: </strong>Enjoy access to new developments to our technology as they are deployed, ensuring you stay ahead of the competition.  </p>
                                 <button onClick={() => setModalShow(true)} className='emp-btn fw-bold text-black fs-3 w-100 md:my-5 mb-0 mt-5 display-btn'>PROVIDE MORE VALUE</button>
                             </Col>
-
                         </Row>
                     </Container>
 
                 </div>
 
-                <div className='earn-commision md:py-5 py-3 custom-sections software-solution'>
+                <div className='earn-commision custom-sections software-solution'>
                     <Container>
                         <Row className="mt-3 align-items-center px-2 md-px-0">
                         <Col md={7} className=' pt-3 pt-md-0 '>
@@ -82,11 +74,11 @@ const SoftwareSolutions = () => {
                                     <strong className='text-white'>Feature Requests: </strong>
                                     Work with our team to develop your technology stack based on your users needs.</p>
                                 
-                                <button onClick={() => setModalShow(true)} className='emp-btn fw-bold text-black fs-3 w-100 my-5 display-btn'>PROVIDE MORE VALUE</button>
+                                <button className='emp-btn fw-bold text-black fs-3 w-100 my-5 display-btn'>PROVIDE MORE VALUE</button>
                             </Col>
                             <Col md={5} className='pe-md-5  text-center'>
                                 <img className='w-75' src={Rating} />
-                                <button onClick={() => setModalShow(true)} className='emp-btn fw-bold text-black fs-3 w-100 md:my-5 mb-0 mt-5 display-btn-new BACKED'>BACKED BY HOSTBUDDY AI</button></Col>
+                                <button className='emp-btn fw-bold text-black fs-3 w-100 md:my-5 mb-0 mt-5 display-btn-new BACKED'>BACKED BY HOSTBUDDY AI</button></Col>
                         </Row>
                     </Container>
 
@@ -136,18 +128,16 @@ const SoftwareSolutions = () => {
 
                                     </div>
                                 </div>
-                                <button onClick={() => setModalShow(true)} className='emp-btn fw-bold text-black fs-3 w-100 md:my-5 mb-0 mt-5 display-btn'>BECOME AN AFFILIATE</button>
 
                             </Col>
                             <Col md={5} className=' ps-md-5 text-center custom-order-1'>
                                 <img className='w-75' src={BecomeAff} />
-                                <button onClick={() => setModalShow(true)} className='emp-btn fw-bold text-black fs-3 w-100 md:my-5 mb-0 mt-5 display-btn-new'>BOOK A DEMO</button>
+                                <button onClick={() => window.open('https://calendly.com/sam-hostbuddy/30min', '_blank')} className='emp-btn fw-bold text-black fs-3 w-100 md:my-5 mb-0 mt-5 display-btn-new'>BOOK A DEMO</button>
                             </Col>
                         </Row>
                     </Container >
                 </div >
             </div>
-            <ContactUs show={modalShow} onHide={() => setModalShow(false)} />
         </>
     )
 }
