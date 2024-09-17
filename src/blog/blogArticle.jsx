142980d19d2f8d433a6bbf7e9bf88e5600c5433a
Armando <jgrados96@gmail.com>
2024-09-17 06:56:26 -0500

    blog-post-and-seo-updates

--- src/blog/blogArticle.jsx
+++ src/blog/blogArticle.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import FiveBestPMS from './blogArticles/5_best_PMS_6-13';
 import automateStr from './blogArticles/automate_str_6-25';
+import ownerRezPmsPartners from './blogArticles/ownerRez_pms_partners';
 
 const BlogArticle = () => {
   const { article_name } = useParams(); // Get the article_name from the path param
@@ -10,6 +11,7 @@ const BlogArticle = () => {
   const name_to_article_mapping = {
     "5_best_PMS_6-13": FiveBestPMS,
     "automate_str_6-25": automateStr,
+    "ownerRez_pms_partners": ownerRezPmsPartners,
   };
 
   useEffect(() => {
