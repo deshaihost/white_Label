d0d3bcdb2feccfb5a06d55bbe7225980bed6cdad
webmanjot01 <webmanjot01@gmail.com>
2024-10-11 17:53:42 +0530

    worked latest smart templates functionality done

--- src/pages/inbox/inboxSection/smartTemplates/smartTemplateFunctionality/smartTemplateAddEdit/SmartTemplateAddEditForm.jsx
+++ src/pages/inbox/inboxSection/smartTemplates/smartTemplateFunctionality/smartTemplateAddEdit/SmartTemplateAddEditForm.jsx
@@ -2,124 +2,344 @@ import React, { useState } from "react";
 import TriggersTrargetsConditionsModel from "./TriggersTrargetsConditionsModel";
 import { dataInput } from "./SmartTemplateJson";
 
-const SmartTemplateAddEditForm = ({ addEditClose }) => {
-  const { triggers } = dataInput;
-  const trigger = "Trigger";
-  const targets = "Targets";
-  const conditions = "Conditions";
+const SmartTemplateAddEditForm = ({
+  addEditSmart,
+  addEditClose,
+  AllDataGetHndle,
+}) => {
+  const { type, smartTemplateData } = addEditSmart;
+  const { triggers, targets, conditions } = dataInput;
+  const edit = "Edit";
+  const add = "Add";
+  const triggerName = "Trigger";
+  const targetsName = "Targets";
+  const conditionsName = "Conditions";
   const [allData, setAllData] = useState({
     modelShow: false,
     modelShowType: "",
     formData: [],
   });
 
-  const [dataStructure, setDataStructure] = useState({
-    name: "hello",
-    is_enabled: true,
-    triggers: [],
-    targets: [],
-    conditions: [],
-  });
-  console.log(dataStructure,'dataStructuredataStructure')
-  const submitHndle = (getFormData) => {
+  const dataStructurePayload = smartTemplateData?.smartItem
+    ? smartTemplateData?.smartItem
+    : {
+        name: "",
+        is_enabled: false,
+        triggers: [],
+        targets: [],
+        conditions: [],
+      };
+  const [dataStructure, setDataStructure] = useState(dataStructurePayload);
 
-    const { data, type,id } = getFormData;
-    console.log(getFormData,'getFormDatagetFormData')
+  const submitHndle = (getFormData) => {
+    const { data, type, editAddTypeSubmitHndle, modelShowType } = getFormData;
+    const { typepAddEdit, editIndex } = editAddTypeSubmitHndle || {};
     const newTrigger = {
-      id,
       type,
       data,
     };
-    setDataStructure((prevData) => ({
-      ...prevData,
-      triggers: [...prevData.triggers, newTrigger],
-    }));
+    if (modelShowType === triggerName) {
+      setDataStructure((prevData) => {
+        if (typepAddEdit === add) {
+          return {
+            ...prevData,
+            triggers: [...prevData.triggers, newTrigger],
+          };
+        } else if (typepAddEdit === edit) {
+          const updatedTriggers = [...prevData.triggers];
+          if (editIndex >= 0 && editIndex < updatedTriggers.length) {
+            updatedTriggers[editIndex] = newTrigger;
+          }
+          return {
+            ...prevData,
+            triggers: updatedTriggers,
+          };
+        }
+      });
+    } else if (modelShowType === targetsName) {
+      setDataStructure((prevData) => {
+        if (typepAddEdit === add) {
+          return {
+            ...prevData,
+            targets: [...prevData.targets, newTrigger],
+          };
+        } else if (typepAddEdit === edit) {
+          const updatedTriggers = [...prevData.targets];
+          if (editIndex >= 0 && editIndex < updatedTriggers.length) {
+            updatedTriggers[editIndex] = newTrigger;
+          }
+          return {
+            ...prevData,
+            targets: updatedTriggers,
+          };
+        }
+      });
+    } else if (modelShowType === conditionsName) {
+      setDataStructure((prevData) => {
+        if (typepAddEdit === add) {
+          return {
+            ...prevData,
+            conditions: [...prevData.conditions, newTrigger],
+          };
+        } else if (typepAddEdit === edit) {
+          const updatedTriggers = [...prevData.conditions];
+          if (editIndex >= 0 && editIndex < updatedTriggers.length) {
+            updatedTriggers[editIndex] = newTrigger;
+          }
+          return {
+            ...prevData,
+            conditions: updatedTriggers,
+          };
+        }
+      });
+    }
   };
 
   return (
     <div>
-      <h1>Edit Smart Template</h1>
-      <p onClick={addEditClose}>smart Templates </p>
-      <div>
-        <p>
+      <div className="d-flex gap-3 flex-wrap align-items-center justify-content-between mb-3">
+        <div>
+          <h1>{type?.type} Smart Template</h1>
+          <p onClick={addEditClose} className="text-primary">{"<"} Smart Templates </p>
+        </div>
+
+        <button
+          className="bg_theme_btn mb-3"
+          onClick={() => {
+            AllDataGetHndle(dataStructure, type);
+            addEditClose();
+          }}
+        >
+          Save
+        </button>
+      </div>
+      <hr className="bg-white opacity-100 my-4" style={{ height: "2px" , opacity:'75%'}} />
+      <div className="my-3">
+        <p className="d-flex align-items-center gap-5">
           Enable Post Stay Review Request
-          <div class="form-check form-switch text-white">
+          <div class="form-check form-switch">
             <input
-              class="form-check-input"
+              className="form-check-input"
               type="checkbox"
+              checked={dataStructure?.is_enabled}
+              onClick={(e) => {
+                setDataStructure({
+                  ...dataStructure,
+                  is_enabled: e.target.checked,
+                });
+              }}
               id="flexSwitchCheckChecked"
             />
           </div>
         </p>
+        <p className="fs-14 text-muted">
+          You currently have post-stay review requests{" "}
+          <span
+            className={
+              dataStructure?.is_enabled ? "text-success" : "text-danger"
+            }
+          >
+            {dataStructure?.is_enabled ? "enabled" : "disable"}
+          </span>
+        </p>
       </div>
-      <div>
-        <p>Template Name</p>
-        <input type="text" />
+      <div className="row">
+        <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+          <p className="fs-5 fw-bold">Template Name</p>
+          <input
+            className="form-control mt-2"
+            type="text"
+            value={dataStructure?.name}
+            onChange={(e) => {
+              setDataStructure({ ...dataStructure, name: e.target.value });
+            }}
+          />
+        </div>
       </div>
-      <div className="my-5">
-        <p>Triggers</p>
-        <p>This controls what will cause the message to be send</p>
+      <div className="my-3">
+        <p className="fs-5 fw-bold">Triggers</p>
+        <p className="fs-14 mt-1 text-muted">
+          This controls what will cause the message to be send
+        </p>
         {dataStructure?.triggers?.length > 0 &&
           dataStructure?.triggers?.map((trigger, index) => {
             const { type } = trigger;
             return (
-              <div>
-                {type}{" "}
-                <span className="mx-3" onClick={() =>
-            setAllData({
-              modelShow: true,
-              modelShowType: "edit",
-              formData: triggers,
-              editFormData:trigger,
-            })
-          } ><i class="bi bi-pencil"></i></span>
-                <span className="ms-3">
-                  <i
-                    class="bi bi-trash3"
-                    onClick={() => {
-                      setDataStructure((prevData) => ({
-                        ...prevData,
-                        triggers: dataStructure?.triggers?.filter(
-                          (_, id) => id !== index
-                        ),
-                      }));
-                    }}
-                  ></i>
-                </span>
+              <div className="row">
+                <div className="col-lg-4">
+                  <div className="d-flex align-items-center justify-content-between gap-2 mt-2">
+                    <p className="fs-6">{type}</p>
+
+                    <div className="d-flex align-items-center gap-3">
+                      <p
+                        className="text-danger mainCursor fs-6"
+                        onClick={() => {
+                          setDataStructure((prevData) => ({
+                            ...prevData,
+                            triggers: dataStructure?.triggers?.filter(
+                              (_, id) => id !== index
+                            ),
+                          }));
+                        }}
+                      >
+                        Remove
+                      </p>
+                      <p
+                        onClick={() =>
+                          setAllData({
+                            modelShow: true,
+                            modelShowType: triggerName,
+                            formData: triggers,
+                            editFormData: trigger,
+                            editIndex: index,
+                            typepAddEdit: edit,
+                          })
+                        }
+                        className="text-primary mainCursor fs-6"
+                      >
+                        Edit
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             );
           })}
         <button
+          className="bg-none text-primary border-0 outline-0 fs-6 fw-bold px-2 mt-1 d-flex align-items-center"
           onClick={() =>
             setAllData({
               modelShow: true,
-              modelShowType: trigger,
+              modelShowType: triggerName,
               formData: triggers,
+              typepAddEdit: add,
             })
           }
         >
-          Add a Trigger
+          <i className="bi bi-plus fs-3"></i> Add a Trigger
         </button>
       </div>
-      <div className="my-5">
-        <p>Targets</p>
-        <p>These are the guests that will receive the message</p>
+      <div className="my-3">
+        <p className="fs-5 fw-bold">Targets</p>
+        <p className="fs-14 mt-1 text-muted">
+          These are the guests that will receive the message
+        </p>
+        {dataStructure?.targets?.length > 0 &&
+          dataStructure?.targets?.map((targetsItem, index) => {
+            const { type } = targetsItem;
+            return (
+              <div className="row">
+                <div className="col-lg-4">
+                  <div className="d-flex align-items-center justify-content-between gap-2 mt-2">
+                    <p className="fs-6">{type}</p>
+
+                    <div className="d-flex align-items-center gap-3">
+                      <p
+                        className="text-danger mainCursor fs-6"
+                        onClick={() => {
+                          setDataStructure((prevData) => ({
+                            ...prevData,
+                            targets: dataStructure?.targets?.filter(
+                              (_, id) => id !== index
+                            ),
+                          }));
+                        }}
+                      >
+                        Remove
+                      </p>
+                      <p
+                        onClick={() =>
+                          setAllData({
+                            modelShow: true,
+                            modelShowType: targetsName,
+                            formData: targets,
+                            editFormData: targetsItem,
+                            editIndex: index,
+                            typepAddEdit: edit,
+                          })
+                        }
+                        className="text-primary mainCursor fs-6"
+                      >
+                        Edit
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         <button
+          className="bg-none text-primary border-0 outline-0 fs-6 fw-bold px-2 mt-1 d-flex align-items-center"
           onClick={() =>
-            setAllData({ modelShow: true, modelShowType: targets })
+            setAllData({
+              modelShow: true,
+              modelShowType: targetsName,
+              formData: targets,
+              typepAddEdit: add,
+            })
           }
         >
-          Add a Target
+          <i className="bi bi-plus fs-3 "></i> Add a Target
         </button>
       </div>
-      <div className="my-5">
-        <p>Conditions</p>
+      <div className="my-3">
+        <p className="fs-5 fw-bold">Conditions</p>
+        {dataStructure?.conditions?.length > 0 &&
+          dataStructure?.conditions?.map((conditionsItem, index) => {
+            const { type } = conditionsItem;
+            return (
+              <div className="row">
+                <div className="col-lg-4">
+                  <div className="d-flex align-items-center justify-content-between gap-2 mt-2">
+                    <p className="fs-6">{type}</p>
+
+                    <div className="d-flex align-items-center gap-3">
+                      <p
+                        className="text-danger mainCursor fs-6"
+                        onClick={() => {
+                          setDataStructure((prevData) => ({
+                            ...prevData,
+                            conditions: dataStructure?.conditions?.filter(
+                              (_, id) => id !== index
+                            ),
+                          }));
+                        }}
+                      >
+                        Remove
+                      </p>
+                      <p
+                        onClick={() =>
+                          setAllData({
+                            modelShow: true,
+                            modelShowType: conditionsName,
+                            formData: conditions,
+                            editFormData: conditionsItem,
+                            editIndex: index,
+                            typepAddEdit: edit,
+                          })
+                        }
+                        className="text-primary mainCursor fs-6"
+                      >
+                        Edit
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         <button
+          className="bg-none text-primary border-0 outline-0 fs-6 fw-bold px-2 mt-1 d-flex align-items-center"
           onClick={() =>
-            setAllData({ modelShow: true, modelShowType: conditions })
+            setAllData({
+              modelShow: true,
+              modelShowType: conditionsName,
+              formData: conditions,
+              typepAddEdit: add,
+            })
           }
         >
-          Add a Conditions
+          <i className="bi bi-plus fs-3 "></i> Add a Conditions
         </button>
       </div>
       <TriggersTrargetsConditionsModel
--- src/pages/inbox/inboxSection/smartTemplates/smartTemplateFunctionality/smartTemplateAddEdit/SmartTemplateJson.jsx
+++ src/pages/inbox/inboxSection/smartTemplates/smartTemplateFunctionality/smartTemplateAddEdit/SmartTemplateJson.jsx
@@ -1,17 +1,59 @@
 let number = "number";
 let select = "select";
+let multiSelecter = "multiSelecter";
+let time = "time";
+const weeksData = [
+  {
+    label: "Monday",
+    value: "monday",
+  },
+  {
+    label: "Tuesday",
+    value: "tuesday",
+  },
+  {
+    label: "Wednesday",
+    value: "wednesday",
+  },
+  {
+    label: "Thursday",
+    value: "thursday",
+  },
+  {
+    label: "Friday",
+    value: "friday",
+  },
+  {
+    label: "Saturday",
+    value: "saturday",
+  },
+  {
+    label: "Sunday",
+    value: "sunday",
+  },
+];
 export const dataInput = {
   triggers: [
     {
-      guesttype: "checks in",
+      guesttype: "Please Select One",
+      type: "",
+      inputFiled: [],
+    },
+    {
+      guesttype: "When a guest checks in",
       type: "check_in",
       inputFiled: [
         { inputLabel: "Hours", type: number, payloadType: "hours" },
-        { inputLabel: "Minustes", type: number, payloadType: "minutes" },
+        { inputLabel: "Minutes", type: number, payloadType: "minutes" },
         {
           type: select,
           payloadType: "before_or_after",
           inputLabel: [
+            {
+              selectLabel: "Please Select One",
+              type: "seleter",
+              value: "",
+            },
             {
               selectLabel: "Before guest check-in",
               type: "seleter",
@@ -27,22 +69,27 @@ export const dataInput = {
       ],
     },
     {
-      guesttype: "checks out",
+      guesttype: "When a guest checks out",
       type: "check_out",
       inputFiled: [
         { inputLabel: "Hours", type: number, payloadType: "hours" },
-        { inputLabel: "Minustes", type: number, payloadType: "minutes" },
+        { inputLabel: "Minutes", type: number, payloadType: "minutes" },
         {
           type: select,
           payloadType: "before_or_after",
           inputLabel: [
             {
-              selectLabel: "Before guest check-in",
+              selectLabel: "Please Select One",
+              type: "seleter",
+              value: "",
+            },
+            {
+              selectLabel: "Before guest check-out",
               type: "seleter",
               value: "before",
             },
             {
-              selectLabel: "After guest check-in",
+              selectLabel: "After guest check-out",
               type: "seleter",
               value: "after",
             },
@@ -51,11 +98,266 @@ export const dataInput = {
       ],
     },
     {
-      guesttype: "books",
+      guesttype: "When a guest books",
       type: "guest_booked",
       inputFiled: [
         { inputLabel: "Hours", type: number, payloadType: "hours_after" },
-        { inputLabel: "Minustes", type: number, payloadType: "minutes_after" },
+        { inputLabel: "Minutes", type: number, payloadType: "minutes_after" },
+      ],
+    },
+    {
+      guesttype: "Daily",
+      type: "daily",
+      inputFiled: [{ inputLabel: "Time", type: time, payloadType: "time" }],
+    },
+    {
+      guesttype: "Weekly",
+      type: "weekly",
+      inputFiled: [
+        {
+          type: multiSelecter,
+          payloadType: "weekdays",
+          inputLabel: weeksData,
+        },
+        { inputLabel: "Time", type: time, payloadType: "time" },
+      ],
+    },
+    {
+      guesttype: "Monthly",
+      type: "monthly",
+      inputFiled: [
+        {
+          inputLabel: "Day of month",
+          type: number,
+          min: 1,
+          max: 31,
+          onlyUsed: "month1to31",
+          payloadType: "day_of_month",
+        },
+        { inputLabel: "Time", type: time, payloadType: "time" },
+      ],
+    },
+    {
+      guesttype: "Yearly",
+      type: "yearly",
+      inputFiled: [
+        {
+          type: select,
+          payloadType: "month",
+          inputLabel: [
+            {
+              selectLabel: "Please Select One",
+              type: "seleter",
+              value: "",
+            },
+            {
+              selectLabel: "January",
+              type: "seleter",
+              value: "january",
+            },
+            {
+              selectLabel: "February",
+              type: "seleter",
+              value: "february",
+            },
+
+            {
+              selectLabel: "March",
+              type: "seleter",
+              value: "march",
+            },
+            {
+              selectLabel: "April",
+              type: "seleter",
+              value: "april",
+            },
+            {
+              selectLabel: "May",
+              type: "seleter",
+              value: "may",
+            },
+            {
+              selectLabel: "June",
+              type: "seleter",
+              value: "june",
+            },
+
+            {
+              selectLabel: "July",
+              type: "seleter",
+              value: "july",
+            },
+            {
+              selectLabel: "August",
+              type: "seleter",
+              value: "august",
+            },
+            {
+              selectLabel: "September",
+              type: "seleter",
+              value: "september",
+            },
+            {
+              selectLabel: "October",
+              type: "seleter",
+              value: "october",
+            },
+            {
+              selectLabel: "November",
+              type: "seleter",
+              value: "november",
+            },
+            {
+              selectLabel: "December",
+              type: "seleter",
+              value: "december",
+            },
+          ],
+        },
+        {
+          inputLabel: "Day of month",
+          type: number,
+          min: 1,
+          max: 31,
+          onlyUsed: "month1to31",
+          payloadType: "day_of_month",
+        },
+        { inputLabel: "Time", type: time, payloadType: "time" },
+      ],
+    },
+    {
+      guesttype: "When cleaning is complete",
+      type: "cleaning_complete",
+      inputFiled: [
+        { inputLabel: "Hours", type: number, payloadType: "hours_after" },
+        { inputLabel: "Minutes", type: number, payloadType: "minutes_after" },
+      ],
+    },
+  ],
+  targets: [
+    {
+      guesttype: "Please Select One",
+      type: "",
+      inputFiled: [],
+    },
+    {
+      guesttype: "Guests that will check in",
+      type: "guests_checking_in",
+      inputFiled: [
+        {
+          inputLabel: "Min Days From Now",
+          type: number,
+          payloadType: "min_days_from_now",
+        },
+        {
+          inputLabel: "Max Days From Now",
+          type: number,
+          payloadType: "max_days_from_now",
+        },
+      ],
+    },
+    {
+      guesttype: "Guests that will check out",
+      type: "guests_checking_out",
+      inputFiled: [
+        {
+          inputLabel: "Min Days From Now",
+          type: number,
+          payloadType: "min_days_from_now",
+        },
+        {
+          inputLabel: "Max Days From Now",
+          type: number,
+          payloadType: "max_days_from_now",
+        },
+      ],
+    },
+    {
+      guesttype: "Guests that have checked in",
+      type: "guests_checked_in",
+      inputFiled: [
+        {
+          inputLabel: "Min Days Ago",
+          type: number,
+          payloadType: "min_days_ago",
+        },
+        {
+          inputLabel: "Max Days Ago",
+          type: number,
+          payloadType: "max_days_ago",
+        },
+      ],
+    },
+    {
+      guesttype: "Guests that have checked out",
+      type: "guests_checked_out",
+      inputFiled: [
+        {
+          inputLabel: "Min Days Ago",
+          type: number,
+          payloadType: "min_days_ago",
+        },
+        {
+          inputLabel: "Max Days Ago",
+          type: number,
+          payloadType: "max_days_ago",
+        },
+      ],
+    },
+    {
+      guesttype: "Guests currently staying",
+      type: "guests_currently_staying",
+      inputFiled: [{}],
+    },
+  ],
+  conditions: [
+    {
+      guesttype: "Please Select One",
+      type: "",
+      inputFiled: [],
+    },
+    {
+      guesttype: "Weekday",
+      type: "is_day_of_week",
+      inputFiled: [
+        {
+          type: multiSelecter,
+          payloadType: "weekdays",
+          inputLabel: weeksData,
+        },
+        // { inputLabel: "Time", type: time, payloadType: "time" },
+      ],
+    },
+    {
+      guesttype: "Time Range",
+      type: "is_within_time_range",
+      inputFiled: [
+        { inputLabel: "Start", type: time, payloadType: "start_time" },
+        { inputLabel: "End", type: time, payloadType: "end_time" },
+      ],
+    },
+    {
+      guesttype: "Sentiment",
+      type: "sentiment",
+      inputFiled: [
+        {
+          type: multiSelecter,
+          payloadType: "criteria",
+          inputLabel: [
+            {
+              label: "negative",
+              value: "negative",
+            },
+            {
+              label: "neutral",
+              value: "neutral",
+            },
+            {
+              label: "positive",
+              value: "positive",
+            },
+          ],
+        },
       ],
     },
   ],
