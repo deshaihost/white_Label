5b4edb621089401a3837d1b6c3d561190dc6c454
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-10-27 23:06:34 -0700

    Change missing info tag in action items

--- src/pages/actionItems/actionItemsTable/ActionsItemsTable.jsx
+++ src/pages/actionItems/actionItemsTable/ActionsItemsTable.jsx
@@ -196,7 +196,7 @@ const ActionsItemsTable = () => {
                   <option value="MAINTENANCE">Maintenance</option>
                   <option value="RESERVATION CHANGES">Reservation Changes</option>
                   <option value="GUEST REQUESTS">Guest Requests</option>
-                  <option value="MISSING INFO">Missing Info</option>
+                  <option value="KNOWLEDGE BASE SUGGESTIONS">Knowledge Base Suggestions</option>
                   <option value="OTHER">Other</option>
                 </select>
               </div>
