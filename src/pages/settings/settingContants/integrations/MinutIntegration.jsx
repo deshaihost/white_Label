1c32b4fc6d924be4b7d340da101d5aa47e3ff2bc
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-11-06 14:44:24 -0800

    Push Minut integration button so I can test it

---
+++ src/pages/settings/settingContants/integrations/MinutIntegration.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MinutIntegration = () => {
+  return (
+    <div>
+      {/* Minut integration functionality (to be implemented) */}
+    </div>
+  );
+};
+
+export default MinutIntegration;
