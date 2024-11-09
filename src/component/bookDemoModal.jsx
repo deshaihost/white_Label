28fc277462d42bb4c22d0ba8c695cc9d56f49ad1
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-11-09 13:56:47 -0800

    Change group demo cutoff to 15

--- src/component/bookDemoModal.jsx
+++ src/component/bookDemoModal.jsx
@@ -85,7 +85,7 @@ const BookDemoModal = ({show, onHide, sourceMsg}) => {
     //let url = randomlySelectedDemoPerson.url;
     let url = 'https://calendly.com/d/ckq2-5yb-8f5/hostbuddy-ai-demo'; // big customer demo
 
-    if (parseInt(formData.propertyCount) <= 25) {
+    if (parseInt(formData.propertyCount) <= 15) {
       url = 'https://calendly.com/nick-hostbuddy/hostbuddy-ai-group-demo';
     }
 
