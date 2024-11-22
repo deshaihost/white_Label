ca75ea2bafed130696bca0a2f4c0ba6e5e4fe7a2
MBoddie-Hostbuddy <michaelboddie@hostbuddy.ai>
2024-11-21 16:40:38 -0800

    Add sample images for chatbot link and embed

--- src/pages/properties/listIntegrationProperties/embedModal/ChatbotLinkContent.jsx
+++ src/pages/properties/listIntegrationProperties/embedModal/ChatbotLinkContent.jsx
@@ -1,12 +1,19 @@
-
 import React from "react";
 import CopyableCode from "./copyableCode";
+
+const linkImg = 'https://hostbuddylb.com/embed-doc/link.webp';
 
 const ChatbotLinkContent = ({ chatbotKey }) => {
   return (
     <div className="embed-modal-content">
+      <h2>Share a HostBuddy Chat Link With Your Guests</h2>
       <div className='embed-modal-section' style={{marginTop:'10px'}}>
-        <h2>Shareable Chat Link</h2>
+        <div className="widget-preview-images">
+          <div>
+            <img src={linkImg} alt="Chat link preview" />
+            <p className="image-caption">Chat Link Preview</p>
+          </div>
+        </div>
         <p>
           <strong>
             This is a link to a HostBuddy chat window for this property that you can share with your guests.
