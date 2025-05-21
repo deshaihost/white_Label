2b0b7debbbbcd2db68ce71f5b1f9e3aea4a705db
deshaihost <deshai@hostbuddy.ai>
2025-05-21 23:06:00 +0530

    Update MildeSection.jsx with hidden send options

--- src/pages/inbox/inboxSection/inbox/rightSection/RightSection.jsx
+++ src/pages/inbox/inboxSection/inbox/rightSection/RightSection.jsx
@@ -892,7 +892,7 @@ const RightSection = ({ rightSectionData, updateConversationFromApi, setCurrentV
         }}></div>
 
         {/* Contact Information Section */}
-        <div style={{ marginBottom: '15px' }}>
+        {/* <div style={{ marginBottom: '15px' }}>
           <div style={{ 
             display: 'flex', 
             justifyContent: 'space-between', 
@@ -958,7 +958,7 @@ const RightSection = ({ rightSectionData, updateConversationFromApi, setCurrentV
               {contactInfo.email}
             </span>
           </div>
-        </div>
+        </div> */}
 
         <div style={{ 
           borderBottom: '1px solid rgba(255, 255, 255, 0.1)', 
