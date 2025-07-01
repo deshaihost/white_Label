c72df914b8161961d969a7611b6f4c23324dabc9
deshaihost <deshai@hostbuddy.ai>
2025-07-01 07:04:18 +0530

    Fixed total removal frmo the feature page in subscription

--- src/pages/settings/settingContants/subscription/features/SubscriptionFeatures.jsx
+++ src/pages/settings/settingContants/subscription/features/SubscriptionFeatures.jsx
@@ -492,7 +492,7 @@ const SubscriptionFeatures = ({
                     </div>
                     {numProperties > 1 && (
                       <div style={{ fontSize: '14px', fontFamily: 'Samsung Sharp Sans', fontWeight: '500', color: '#FFFFFF' }}>
-                        {formatPriceAsUSD(proPriceTotal)} {billingPeriod === 'annual' ? 'yearly' : 'monthly'} total
+                        {formatPriceAsUSD(proPriceTotal)} {billingPeriod === 'annual' ? 'yearly' : 'monthly'} 
                       </div>
                     )}
                   </div>
@@ -514,7 +514,7 @@ const SubscriptionFeatures = ({
                     </div>
                     {numProperties > 1 && (
                       <div style={{ fontSize: '14px', fontFamily: 'Samsung Sharp Sans', fontWeight: '500', color: '#FFFFFF' }}>
-                        {formatPriceAsUSD(elitePriceTotal)} {billingPeriod === 'annual' ? 'yearly' : 'monthly'} total
+                        {formatPriceAsUSD(elitePriceTotal)} {billingPeriod === 'annual' ? 'yearly' : 'monthly'} 
                       </div>
                     )}
                   </div>
@@ -536,7 +536,7 @@ const SubscriptionFeatures = ({
                     </div>
                     {numProperties > 1 && (
                       <div style={{ fontSize: '14px', fontFamily: 'Samsung Sharp Sans', fontWeight: '500', color: '#FFFFFF' }}>
-                        {formatPriceAsUSD(ultimatePriceTotal)} {billingPeriod === 'annual' ? 'yearly' : 'monthly'} total
+                        {formatPriceAsUSD(ultimatePriceTotal)} {billingPeriod === 'annual' ? 'yearly' : 'monthly'} 
                       </div>
                     )}
                   </div>
