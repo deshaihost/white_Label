9abfe4376a857e03e518fdb9b7036f7a1aecf86d
deshaihost <deshai@hostbuddy.ai>
2025-07-22 21:41:41 +0530

    fencing

--- src/helper/Authorized.js
+++ src/helper/Authorized.js
@@ -124,8 +124,10 @@ export const getSubscriptionStatus = (userData) => {
 
     return { plan: planName, props_allowed: propsAllowed, status: subscrStatus };
   }
-// "HostBuddy Ultimate"
-
+// "HostBuddy ultimate"
+// "HostBuddy pRo"
+// "HostBuddy ELiTe"
+// "HostBuddy mOUNT"
 
   // If no plan name, check if trial applies
   if ('trial_ends' in userData) {
