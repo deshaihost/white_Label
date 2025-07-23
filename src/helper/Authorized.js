36710eb6049246fd5cd4585034f6626a9320c888
deshaihost <deshai@hostbuddy.ai>
2025-07-23 16:40:47 +0530

    mount view mode as pro

--- src/helper/Authorized.js
+++ src/helper/Authorized.js
@@ -131,7 +131,7 @@ export const getSubscriptionStatus = (userData) => {
     //   }
     // }
 
-    return { plan: "HostBuddy mOUNT"  , props_allowed: propsAllowed, status: subscrStatus };
+    return { plan: planName, props_allowed: propsAllowed, status: subscrStatus };
   }
 // "HostBuddy ultimate"
 // "HostBuddy pRo"
